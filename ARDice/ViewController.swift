24f11c8d09d887560190989590a5dfdb30473216
Winton <vinda9219@163.com>
2018-08-29 23:38:24 +1000

    Enable to roll and remove all dices.

--- ARDice/ViewController.swift
+++ ARDice/ViewController.swift
@@ -12,6 +12,8 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
+    var diceArray = [SCNNode]()
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -83,25 +85,60 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                         y: hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
                         z: hitResult.worldTransform.columns.3.z
                       )
-                
+                    
+                     diceArray.append(diceNode)
+                    
                      sceneView.scene.rootNode.addChildNode(diceNode)
                     
-                     let randomX = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
-                    
-                     let randomZ = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
-                    
-                     diceNode.runAction(
-                           SCNAction.rotateBy(
-                                x: CGFloat(randomX * 5),
-                                y: 0,
-                                z: CGFloat(randomZ * 5),
-                                duration: 0.5)
-                     )
+                     roll(dice: diceNode)
                  }
                 
             }
         }
     }
+    
+    func rollAll(){
+        
+        if !diceArray.isEmpty{
+            for dice in diceArray{
+                roll(dice: dice)
+            }
+        }
+        
+    }
+    
+    func roll(dice: SCNNode){
+        let randomX = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
+        
+        let randomZ = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
+        
+        dice.runAction(
+            SCNAction.rotateBy(
+                x: CGFloat(randomX * 5),
+                y: 0,
+                z: CGFloat(randomZ * 5),
+                duration: 0.5)
+        )
+    }
+   
+    @IBAction func rollAgain(_ sender: UIBarButtonItem) {
+        rollAll()
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        rollAll()
+    }
+    
+    @IBAction func removeAllDice(_ sender: UIBarButtonItem) {
+        
+        if !diceArray.isEmpty{
+            for dice in diceArray{
+                dice.removeFromParentNode()
+            }
+        }
+        
+    }
+    
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         if anchor is ARPlaneAnchor{
